ef600f13a5baa1837b01dc0e204294035ea5d4da
Bill Chung <bill_chungiii@hotmail.com>
2020-10-26 05:50:58 +0800

    排版

--- BeautifulWordSample002/BeautifulWordExtension.cs
+++ BeautifulWordSample002/BeautifulWordExtension.cs
@@ -17,15 +17,16 @@ namespace BeautifulWordSample002
             int current = 0;
 
             return source.ToLower()
-                 .OrderByDescending(x => x)
-                 .GroupBy(x => x)
-                 .Select(x =>
-                 {
-                     var count = x.Count();
-                     var result = count - current;
-                     current = count;
-                     return result;
-                 }).All(x => x > 0);
+                         .OrderByDescending(x => x)
+                         .GroupBy(x => x)
+                         .Select(x =>
+                         {
+                             var count = x.Count();
+                             var result = count - current;
+                             current = count;
+                             return result;
+                         })
+                         .All(x => x > 0);
         }
     }
 }

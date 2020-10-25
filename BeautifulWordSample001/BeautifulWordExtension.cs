7880e8c0056d98762041bff9521b369dfafa8e88
Bill Chung <bill_chungiii@hotmail.com>
2020-10-26 04:55:06 +0800

    改成 expression body method

--- BeautifulWordSample001/BeautifulWordExtension.cs
+++ BeautifulWordSample001/BeautifulWordExtension.cs
@@ -8,11 +8,9 @@ namespace BeautifulWordSample001
 {
     public static class BeautifulWordExtension
     {
-        public static bool IsBeautiful(this string source)
-        {
-            return Enumerable.Repeat(source.ToLower().OrderBy(x => x).GroupBy(x => x).Select(x => x.Count()), 2)
-                             .Aggregate((x, y) => x.Zip(y.Skip(1), (a, b) => a - b))
-                             .All(x => x > 0);
-        }
+        public static bool IsBeautiful(this string source) =>
+            Enumerable.Repeat(source.ToLower().OrderBy(x => x).GroupBy(x => x).Select(x => x.Count()), 2)
+                .Aggregate((x, y) => x.Zip(y.Skip(1), (a, b) => a - b))
+                .All(x => x > 0);
     }
 }

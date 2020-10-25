4f184f497b8459a96ff970fbc7bad79c9b753976
Bill Chung <bill_chungiii@hotmail.com>
2020-10-26 04:55:56 +0800

    排版

--- BeautifulWordSample001/BeautifulWordExtension.cs
+++ BeautifulWordSample001/BeautifulWordExtension.cs
@@ -10,7 +10,7 @@ namespace BeautifulWordSample001
     {
         public static bool IsBeautiful(this string source) =>
             Enumerable.Repeat(source.ToLower().OrderBy(x => x).GroupBy(x => x).Select(x => x.Count()), 2)
-                .Aggregate((x, y) => x.Zip(y.Skip(1), (a, b) => a - b))
-                .All(x => x > 0);
+                      .Aggregate((x, y) => x.Zip(y.Skip(1), (a, b) => a - b))
+                      .All(x => x > 0);
     }
 }

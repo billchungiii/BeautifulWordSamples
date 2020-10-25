22bf176dace96d3aa4aa0fce4bf8715f676822e2
Bill Chung <bill_chungiii@hotmail.com>
2020-10-26 03:44:43 +0800

    加入專案檔案。

---
+++ BeautifulWordSample001/BeautifulWordExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulWordSample001
+{
+    public static class BeautifulWordExtension
+    {
+        public static bool IsBeautiful(this string source)
+        {
+            return Enumerable.Repeat(source.ToLower().OrderBy(x => x).GroupBy(x => x).Select(x => x.Count()), 2)
+                 .Aggregate((x, y) => x.Zip(y.Skip(1), (a, b) => a - b)).All(x => x > 0);
+        }
+    }
+}
---
+++ BeautifulWordSample001Tests/BeautifulWordExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeautifulWordSample001;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BeautifulWordSample001.Tests
+{
+    [TestClass()]
+    public class BeautifulWordExtensionTests
+    {
+        [DataRow ("AA", true)]
+        [DataRow("AAB", true)]
+        [DataRow("BAA", true)]
+        [DataRow("ABB", false)]
+        [DataRow("BBA", false)]
+        [DataRow("AaaBbC", true)]
+        [DataRow("cbBaaA", true)]
+        [DataRow("aBbbc", false)]
+        [DataRow("baac", false)]
+        [DataTestMethod ()]
+        public void IsBeautifulTest(string source, bool result) => Assert.AreEqual(source.IsBeautiful(), result);
+    }
+}

78e7ec9a55bd8323e025b6a55bd762956ba7cf7d
Bill Chung <bill_chungiii@hotmail.com>
2020-10-26 06:25:14 +0800

    新增 Sample003，稍微改一下 Sample002

---
+++ BeautifulWordSample003/BeautifulWordExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulWordSample003
+{
+    public static class BeautifulWordExtension
+    {
+        /// <summary>
+        /// 002 的 減法改為關係運算子
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool IsBeautiful(this string source)
+        {
+            int current = 0;
+
+            return source.ToLower()
+                         .OrderByDescending(x => x)
+                         .GroupBy(x => x)
+                         .Select(x =>
+                         {
+                             var count = x.Count();
+                             var result = count > current;
+                             current = count;
+                             return result;
+                         })
+                         .All(x => x);
+        }
+    }
+}
